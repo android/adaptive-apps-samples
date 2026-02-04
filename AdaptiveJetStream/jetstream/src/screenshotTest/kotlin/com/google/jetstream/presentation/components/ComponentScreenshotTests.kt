e1c595f6d6fd2e17603f747602b4868429e3c681
Don Turner <donturner@google.com>
2026-02-04 09:54:10 +0000

    Add screenshot test for TopBar

--- AdaptiveJetStream/jetstream/src/screenshotTest/kotlin/com/google/jetstream/presentation/components/ComponentScreenshotTests.kt
+++ AdaptiveJetStream/jetstream/src/screenshotTest/kotlin/com/google/jetstream/presentation/components/ComponentScreenshotTests.kt
@@ -26,10 +26,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.android.tools.screenshot.PreviewTest
+import com.google.jetstream.presentation.app.AppState
 import com.google.jetstream.presentation.app.UserAvatar
+import com.google.jetstream.presentation.app.withNavigationSuiteScaffold.TopBar
 
 @PreviewTest
 @Preview
@@ -118,3 +122,14 @@ fun UserAvatarScreenshot() {
         }
     }
 }
+
+@PreviewTest
+@Preview
+@Composable
+fun TopBarPreview(){
+    TopBar(
+        appState = AppState(),
+        navController = NavController(LocalContext.current)
+    )
+}
+
